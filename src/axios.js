218841b43d1ca27b49114c9f012993cf37dc6c64
Dispa <90232952+Dispa1@users.noreply.github.com>
2023-07-30 21:53:08 +0600

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:4444',
+    baseURL: 'dispa-blog-beck-b4cl2y8p3-dispa1.vercel.app',
 });
 
 instance.interceptors.request.use((config) => {
